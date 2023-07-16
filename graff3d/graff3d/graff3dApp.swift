637852c764652c7788a1c3a1f09d59eb2e6c628f
Zoe Friedman <friedmanzoe08@gmail.com>
2023-07-16 15:40:28 +1000

    init xcode project

---
+++ graff3d/graff3d/graff3dApp.swift
@@ -0,0 +1,17 @@
+//
+//  graff3dApp.swift
+//  graff3d
+//
+//  Created by Zoe Friedman on 16/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct graff3dApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
